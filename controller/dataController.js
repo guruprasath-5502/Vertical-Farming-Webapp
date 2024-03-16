bed73c63af4e9e0f876ea58e745a82ee0c72d145
guruprasath-5502 <guruprasath2552@gmail.com>
2024-03-16 15:17:47 +0530

    env

--- controller/dataController.js
+++ controller/dataController.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const twilio = require("twilio")(accountSid, authToken);
+const twilio = require('twilio')(accountSid, authToken);
 
 exports.temperature = async (req, res) => {
   try {
     const msgId = await twilio.messages.create({
-      body: "temperature is high",
-      from: "+19497102251",
-      to: "+916385348238",
+      body: 'temperature is high',
+      from: '+14173743786',
+      to: '+919344528262',
     });
     console.log(msgId);
 
     return res
       .status(200)
-      .json({ message: "Message sent successfully", status: true });
+      .json({ message: 'Message sent successfully', status: true });
   } catch (error) {
     console.log(error);
   }
@@ -25,16 +25,16 @@ exports.temperature = async (req, res) => {
 exports.waterLevel = async (req, res) => {
   try {
     const msgId = await twilio.messages.create({
-      body: "Water Level is low",
-      from: "+19497102251",
-      to: "+916385348238",
+      body: 'Water Level is low',
+      from: '+14173743786',
+      to: '+919344528262',
     });
 
     console.log(msgId);
 
     return res
       .status(200)
-      .json({ message: "Message sent successfully", status: true });
+      .json({ message: 'Message sent successfully', status: true });
   } catch (error) {
     console.log(error);
   }
